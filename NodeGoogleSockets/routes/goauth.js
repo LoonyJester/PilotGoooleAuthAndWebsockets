514a46fce06276357e3ee98f8ac1c2aa53bcf833
mykhaylichenko <mykhaylichenko@PRIVATE-0161>
2016-08-22 18:39:04 +0300

    oauth - first step

---
+++ NodeGoogleSockets/routes/goauth.js
@@ -0,0 +1,30 @@
+﻿var express = require('express');
+var router = express.Router();
+
+
+
+
+
+
+
+
+
+
+
+
+/* GET users listing. */
+router.get('/start', function (req, res) {
+    getAccessToken(oauth2Client,
+        function(p1, p2, p3) {
+            res.send('oauth2Client');
+        });
+//    res.send('start get');
+});
+
+router.get('/goauthcallback', function (req, res) {
+    res.send('get goauthcallback');
+});
+
+
+
+module.exports = router;
--- NodeGoogleSockets/routes/index.js
+++ NodeGoogleSockets/routes/index.js
@@ -1,9 +1,34 @@
 ﻿var express = require('express');
 var router = express.Router();
 
+
+
+
+
+var CLIENT_ID = 'YOUR CLIENT ID HERE';
+var CLIENT_SECRET = 'YOUR CLIENT SECRET HERE';
+var REDIRECT_URL = 'YOUR REDIRECT URL HERE';
+
+var google = require('googleapis');
+var OAuth2 = google.auth.OAuth2;
+
+var oauth2Client = new OAuth2(CLIENT_ID, CLIENT_SECRET, REDIRECT_URL);
+
+var scopes = [
+    'https://www.googleapis.com/auth/plus.me',
+    'https://www.googleapis.com/auth/calendar'
+];
+
+
 /* GET home page. */
 router.get('/', function (req, res) {
-    res.render('index', { title: 'Google and WebSockets' });
+
+    var url = oauth2Client.generateAuthUrl({
+        access_type: 'offline', // 'online' (default) or 'offline' (gets refresh_token) 
+        scope: scopes // If you only need one scope you can pass it as string 
+    });
+
+    res.render('index', { title: 'Google and WebSockets', oauthUrl : url });
 });
 
 module.exports = router;
